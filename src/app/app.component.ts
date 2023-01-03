39dea5dae5bd9a8c776854fd28900dea65ccb3cc
Spyridon PEPPAS <CJ6402@engie.com>
2023-01-03 11:18:34 +0100

    renamed repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-jest-spectator-template';
+  title = 'angular-template-jest-spectator-playwright';
 }
